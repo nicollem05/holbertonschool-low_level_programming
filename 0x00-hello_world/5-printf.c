4e147cb53c362ecdd17fa46ea3465c77cf651ba1
nicollem05 <nicollemunar@gmail.com>
2020-06-10 23:34:16 +0000

    add 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this command returns me a print
+ * Return: this return me 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
