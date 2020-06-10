b0a447876934e9d0908248e3ef77aaa401b74391
nicollem05 <nicollemunar@gmail.com>
2020-06-10 22:56:41 +0000

    add 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this command returns me printf"
+ * Return: this command returns me 0
+ */
+int main(void)
+{
+	puts( "\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
