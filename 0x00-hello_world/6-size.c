a3badd0882939b8f2c1910830af9b2b9dfc6e257
nicollem05 <nicollemunar@gmail.com>
2020-06-11 02:51:48 +0000

    add 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this function returns me the size of the values"
+ * Return: returns me 0
+ */
+int main(void)
+{
+	printf("Size of a char: %lu\n", sizeof(char));
+	printf("Size of an int: %lu\n", sizeof(int));
+	printf("Size of a long int: %lu\n", sizeof(long int));
+	printf("Size of a long long int: %lu\n", sizeof(long long int));
+	printf("Size of a float: %lu\n", sizeof(float));
+	return (0);
+}
