80e9f715d1cdf8684facf2643d75249552f9b85b
nicollem05 <nicollemunar@gmail.com>
2020-06-13 01:43:07 +0000

    add 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main -This programm prints the alphabet backwards
+ *Return: nothing
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}
