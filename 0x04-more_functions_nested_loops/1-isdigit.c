90a35e1b4b777789e9c418b487ea70509b831fea
nicollem05 <nicollemunar@gmail.com>
2020-06-18 21:12:15 +0000

    add 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _isadigit - Checks if the value is a number
+ * @c: int
+ * Return: int 
+ */
+int _isdigit(int c)
+{
+	if((c < 'A') && (c = 'Z'))
+	{
+		return (1);
+	} 
+
+		return (0);
+}
