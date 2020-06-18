b740ccb3cd49a03875784608ba8047a2977f513f
nicollem05 <nicollemunar@gmail.com>
2020-06-18 21:06:39 +0000

    add 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _isupper - Show 1 or 0 when the letter is an uppercase
+ * @c: int
+ * Return: int
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c < 'Z'))
+	{
+		return (1);
+	}
+
+		return (0);
+}
