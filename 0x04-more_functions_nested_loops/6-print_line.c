5b745b2c2eb32a583ed4d916a7abc3c898a2af15
nicollem05 <nicollemunar@gmail.com>
2020-06-19 02:24:20 +0000

    add 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
- * print_line - make a link 
- * @p: a number
+ * print_line - make a line
+ * @n: a number
  * Return: void
  */
 void print_line(int n)
 {
-	int p=0;
+	int p = 0;
 
-	while(p <= n)
+	while (p <= n)
 	{
 		_putchar('_');
 		p++;
