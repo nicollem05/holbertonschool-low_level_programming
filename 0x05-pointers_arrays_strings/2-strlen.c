ce08af6d135da92f8a4fba39df154bd891a3d201
nicollem05 <nicollemunar@gmail.com>
2020-06-23 17:20:02 +0000

    add 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 /**
-  * _strlen - check the code for >Holberton School students
-  * @s: is a pointer
-  * Return: zero
-  */
+ * _strlen - check the code for >Holberton School students
+ * @s: is a pointer
+ * Return: zero
+ */
 int _strlen(char *s)
 {
 	int a;
 
-	for (a = 0; s[a] != '\0'; a++);
+	for (a = 0; s[a] != '\0'; a++)
+		;
 	return (a);
 }
