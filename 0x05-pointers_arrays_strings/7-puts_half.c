d5f6427401d96e0fe570aef0e1339b410f02bf7d
nicollem05 <nicollemunar@gmail.com>
2020-06-24 01:14:19 +0000

    add 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * puts_half - returns me a half of the array
+ * @str: is a pointer
+ * Return: zero
+ */
+void puts_half(char *str)
+{
+	int size;
+	int i;
+
+	for (size = 0; str[size] != '\0'; size++)
+	{
+
+	}
+	for (i = size / 2; i < size; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
