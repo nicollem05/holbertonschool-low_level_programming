d1362e0d64b367bc605ed96fa4031274aee60b7b
nicollem05 <nicollemunar@gmail.com>
2020-06-24 04:09:37 +0000

    add 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - prints and array
+ * @a: is a pointer
+ * @n: int
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int size;
+
+	for (size = 0; size < n; size++)
+	{
+		if(size == n - 1)
+		{
+			printf("%d", a[size]);
+		}
+		else
+		{
+			printf("%d, ", a[size]);
+		}
+	}	printf("\n");
+}
